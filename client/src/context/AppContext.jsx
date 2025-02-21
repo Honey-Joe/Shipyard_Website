3c12a63ce380ca5f453ff73718140005018396b8
Honey-Joe <honeyjoe942@gmail.com>
2025-02-22 01:00:18 +0530

    update the tender get

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,12 @@
-import React, { createContext, useState } from 'react'
+import axios from 'axios';
+import React, { createContext, useState ,useEffect} from 'react'
 // import { teams } from '../assets/api/teams';
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
     // const [team, setTeam] = useState([])
+    const [tenderData, setTenderData] = useState([])
     const name = "SJC"
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [token , setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false )
@@ -12,12 +14,34 @@ const AppContextProvider = (props) => {
         localStorage.removeItem('token')
         setToken(false)
     }
+    const gettenderData = async () => {
+        try {
+            const response = await axios.get(`${backendUrl}/api/tenders`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,	
+                },
+            });
+            
+            console.log(response.data);  
+            setTenderData(response.data)
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+    useEffect(() => {  
+        gettenderData()
+
+    }, [])
 
     const value = {
         name,
         backendUrl,
         token,
         setToken,
+        tenderData,
+        setTenderData,
         logout
     }
   return (
--- client/src/pages/Tender.jsx
+++ client/src/pages/Tender.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Tender = () => {
+  const {tenderData,setTenderData} = useContext(AppContext)
+  console.log(tenderData);
   return (
-    <div className=' pt-20'>Tender</div>
+    <div className=' sm:px-[10%] px-6 pt-28 pb-20'>
+      <div>
+        <div>
+          <p>All tenders</p>
+        </div>
+        <div>
+          {
+            tenderData.map((e,index)=>(
+              <div key={index}>
+                <p>{e.name}</p>
+                <p>{e.deadline}</p>
+                <p>{e.status}</p>
+                <p>{e.description}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+
+    </div>
   )
 }
 
